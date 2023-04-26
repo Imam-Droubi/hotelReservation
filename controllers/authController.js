d3148ce2c6e629b888a6dd41dd683b04f5d906ab
ImamDroubi <imam2002@outlook.sa>
2023-04-27 00:18:34 +0300

    fix2

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,6 +34,7 @@ export const login = async (req,res,next)=>{
     res.cookie("access_token" , token, {
       httpOnly :true,
       secure : true,
+      sameSite : false
     }).status(200).json({...otherDetails});
 
   }catch(err){
