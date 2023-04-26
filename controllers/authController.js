8728fe10ac470b765854cf3cae2a8c258d923bef
ImamDroubi <77539799+ImamDroubi@users.noreply.github.com>
2023-04-27 00:46:03 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,7 +34,7 @@ export const login = async (req,res,next)=>{
     res.cookie("access_token" , token, {
       httpOnly :true,
       secure : true,
-      sameSite : false
+      sameSite : "None",
     }).status(200).json({...otherDetails});
 
   }catch(err){
